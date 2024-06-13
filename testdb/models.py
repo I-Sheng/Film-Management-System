9ab23e9022c7b3f1f8ea26bb568370930b8a1455
isheng <110703011@g.nccu.edu.tw>
2024-06-13 23:26:39 +0800

    make file editable

--- testdb/models.py
+++ testdb/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=200, db_column='move_title')
-    file = models.BinaryField()
+    file = models.BinaryField(editable=True)
 
     class Meta:
         db_table = "movies"
